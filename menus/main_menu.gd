dc0cb1cb1b47d06f0db322eb2581a9689745aea6
Dan D. <daniel.mi.do@gmail.com>
2024-09-01 12:51:23 +0100

    Main menu functionality complete

--- menus/main_menu.gd
+++ menus/main_menu.gd
@@ -3,6 +3,7 @@ extends Control
 @onready var start : Button = %Start
 @onready var level2 : Button = %Night2
 @onready var level3 : Button = %Night3
+@onready var guide : Button = %Guide
 @onready var vol_button : Button = %Volume
 @onready var vol_slider : HSlider = %Volumeslider
 @onready var screen_button : Button = %Screenshake
@@ -18,6 +19,8 @@ func _ready() -> void:
 		level2.disabled = false
 	if Globals.level_reached > 2:
 		level3.disabled = false
+	vol_slider.value = 0.3
+	screen_slider.value = 1.0
 
 func _on_start_pressed() -> void:
 	Globals.setup_level_1()
@@ -33,21 +36,21 @@ func _on_night_3_pressed() -> void:
 	get_tree().change_scene_to_file("res://Main/main.tscn")
 
 func _on_volumeslider_changed() -> void:
-	# TODO
-	pass # Replace with function body.
-
+	AudioServer.set_bus_volume_db(0, linear_to_db(vol_slider.value))
 
 func _on_screenshake_toggled(toggled_on: bool) -> void:
 	screen_slider.visible = toggled_on
+	if toggled_on:
+		guide.button_pressed = false
 
 
 func _on_volume_toggled(toggled_on: bool) -> void:
 	vol_slider.visible = toggled_on
-
+	if toggled_on:
+		guide.button_pressed = false
 
 func _on_screenslider_changed() -> void:
-	# TODO
-	pass # Replace with function body.
+	Globals.screen_shake = screen_slider.value
 
 func _on_fullscreen_toggled(toggled_on: bool) -> void:
 	if toggled_on:
@@ -64,3 +67,9 @@ func _on_3x_toggled(toggled_on: bool) -> void:
 	if toggled_on:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 		get_viewport().size = (base_size * 3)
+
+func _on_guide_toggled(toggled_on: bool) -> void:
+	$PanelContainer.visible = toggled_on
+	if toggled_on:
+		vol_button.button_pressed = false
+		screen_button.button_pressed = false
